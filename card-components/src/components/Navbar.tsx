b21e99f6eeb595e3a28be781f62614f10ba5a161
kunj3740 <kunjdave694@gmail.com>
2024-08-17 14:39:03 +0530

    UI Responsive stuff

--- card-components/src/components/Navbar.tsx
+++ card-components/src/components/Navbar.tsx
@@ -108,7 +108,7 @@ export const Navbar = () => {
             ) : (
               <Link
                 href="/signin"
-                className="text-white ml-14 bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center transition duration-150 ease-in-out mb-2"
+                className="text-white ml-5 bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center transition duration-150 ease-in-out mb-2"
               >
                 Log in
               </Link>
